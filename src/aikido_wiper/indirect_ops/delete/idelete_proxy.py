d3b3c920615e4a9597bd8ffd69988788af0f0aa2
Or Yair <or.yair@safebreach.com>
2022-09-14 11:39:24 +0300

    Added the IDeleteProxy general interface for deletion proxies

---
+++ src/aikido_wiper/indirect_ops/delete/idelete_proxy.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod, ABCMeta
+from typing import Iterable
+
+
+class IDeleteProxy(ABC):
+    """
+    A proxy for deleting a file or a directory on the computer without doing it directly.
+    """
+    
+    @abstractmethod
+    def indirect_delete_paths(self, paths_to_delete: Iterable[str]) -> set[str]:
+        """
+        Using the proxy, trying to delete a list of given paths.
+
+        :param paths_to_delete: The list of paths to try to delete
+        :return: A set of paths that the proxy has failed to delete.
+        """
+        raise NotImplementedError()
+
