9e045d845d47c2f966220349f5baeaf1f1a8e5ce
Or Yair <or.yair@safebreach.com>
2022-09-28 14:01:04 +0300

    Changed a help sentence in the args

--- wiper_tool/configs/args.py
+++ wiper_tool/configs/args.py
@@ -65,8 +65,8 @@ def parse_args():
     all_dirs_parser = delete_target_subparsers.add_parser(DeletionTargetsOptions.ALL_USER_DATA.name, help="Try to delete the home directory of a certain user")
     all_dirs_parser.add_argument("target_user", help="The target user to delete its home directory", type=str)
 
-    all_files_parser = delete_target_subparsers.add_parser(DeletionTargetsOptions.ALL_FILES_UNDER_PATH.name, help="Try to delete all the files under the Windows drive")
-    all_files_parser.add_argument("root_path", help="The parent path for all the file to delete", type=str)
+    all_files_parser = delete_target_subparsers.add_parser(DeletionTargetsOptions.ALL_FILES_UNDER_PATH.name, help="Try to delete all the files under the certain path")
+    all_files_parser.add_argument("root_path", help="The parent path for all the files to delete", type=str)
     all_files_parser.add_argument("--exclusion-list-path", help="Path to a file with a list of paths to exclude and not mark", required=False)
 
     custom_paths_parser = delete_target_subparsers.add_parser(DeletionTargetsOptions.CUSTOM_PATHS.name, help="Try to delete custom paths")
